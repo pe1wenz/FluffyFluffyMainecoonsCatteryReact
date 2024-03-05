071cfafa7646b916bb856344dc5ad018b4a0eedf
pe1wenz <peiwenz@vt.edu>
2024-03-04 21:46:16 -0500

    -create kitten litter photo data folder and files for every litter -now all litter listings are working
    -pz

--- README.md
+++
@@ -1,6 +0,0 @@
-# FluffyFluffyMainecoonsCattery
- fullstack website for a maine coons cattery 
-
-Frontend : React.js
-
-Backend : Springboot Java Maven MySql
--- frontend/src/data/MenuData.tsx
+++
@@ -1,6 +0,0 @@
-export const menuData = [
-    {title: 'Home', link: '/home'},
-    {title: 'Kittens', link: '/kittens'} ,
-    {title: 'Our Cats', link: '/our-cats'}  ,
-    {title: 'Owning', link: '/owning'}  ,
-];
